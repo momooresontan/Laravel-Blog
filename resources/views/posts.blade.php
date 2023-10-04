ab5a6ff20d703549087ffa079422396e79f98140
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-04 15:24:46 +0100

    Blog posts made

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,15 @@
 </head>
 <body>
     <article>
-        <h1>My First Post</h1>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
     </article>
 </body>
