93fa6fc1f7f3dffc6e7e8c5a8c89c1bc9527109f
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-04 17:34:16 +0100

    Go back added & post variable created

---
+++ resources/posts/my-third-post.php
@@ -0,0 +1,3 @@
+<h1><a href="/post">My Third Post</a></h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
+    
