5770d97bafba086f323795be54cf4567039d48f5
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-05 12:34:03 +0100

    caching forever

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,16 +23,18 @@
            return static::all()->firstWhere("slug", $slug);
         }
         public static function all(){
-            return collect(File::files(resource_path("posts")))
-                ->map(fn($file)=> YamlFrontMatter::parseFile($file))    
-                ->map(fn($document)=> new Post(
-                    $document->title, 
-                    $document->excerpt, 
-                    $document->date, 
-                    $document->body(), 
-                    $document->slug, 
-                )
-            )->sortByDesc("date");
+            return cache()->rememberForever("posts.all", function () {
+                return collect(File::files(resource_path("posts")))
+                    ->map(fn($file)=> YamlFrontMatter::parseFile($file))    
+                    ->map(fn($document)=> new Post(
+                        $document->title, 
+                        $document->excerpt, 
+                        $document->date, 
+                        $document->body(), 
+                        $document->slug, 
+                    )
+                )->sortByDesc("date");                
+            });
         }
     }
 ?>
