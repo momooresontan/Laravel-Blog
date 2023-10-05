957eefe5237021cbe6ed96fe49bcf7e8b880154e
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-05 10:15:42 +0100

    using meta data

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,6 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $document = YamlFrontMatter::parseFile(
-        resource_path("posts/my-first-post.php")
-    );
-
-    ddd($document);
-
     // return view("posts", [
     //     "posts" => Post::all()
     // ]);
