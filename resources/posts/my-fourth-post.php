174065c4f88f17601c375e011a2f627ae595f58b
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-05 11:19:47 +0100

    using meta data & slug & map array

--- resources/posts/my-fourth-post.php
+++ resources/posts/my-fourth-post.php
@@ -1,7 +1,7 @@
 ---
 title: My Fourth Post
 excerpt: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-date: 10-04-23
+date: 10-05-23
 slug: my-fourth-post
 ---
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
