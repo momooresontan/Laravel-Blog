13ebb6985aadbfe548f1f64202be672c68c0bf20
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-05 10:36:53 +0100

    using meta data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,18 @@
     use Illuminate\Support\Facades\File;
 
     class Post {
+        public $title;
+        public $excerpt;
+        public $date;
+        public $body;
+
+        public function __construct($title, $excerpt, $date, $body){
+            $this->title = $title;
+            $this->excerpt = $excerpt;
+            $this->date = $date;
+            $this->body = $body;
+        }
+
         public static function find($slug){
             base_path();
             if(!file_exists($path = resource_path("posts/{$slug}.php"))){
--- resources/posts/my-third-post.php
+++ resources/posts/my-third-post.php
@@ -1,3 +1,8 @@
-<h1>My Third Post</h1>
+---
+title: My Third Post
+excerpt: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+date: 10-04-23
+---
+
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
     
