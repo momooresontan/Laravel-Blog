88273fbdbd3f44bcb728a5351ec4c7c0994cc7b6
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-09 14:04:57 +0100

    creating post table

--- resources/posts/my-fifth-post.php
+++
@@ -1,8 +0,0 @@
----
-title: My Fifth Post
-excerpt: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-date: 10-05-23
-slug: my-fifth-post
----
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
-    
--- resources/posts/my-second-post.php
+++
@@ -1,8 +0,0 @@
----
-title: My Second Post
-excerpt: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-date: 10-04-23
-slug: my-second-post
----
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis temporibus tempora saepe perspiciatis recusandae non quisquam repellat ullam atque assumenda cupiditate, earum commodi molestias nemo impedit amet perferendis magni voluptatem!</p>
-    
