8ea8842d8aa4ca61800f4085f6fca3f1af78ade1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-10 17:31:01 +0100

    adding author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            "name"=>"Personal",
+            "slug"=>"personal",    
+        ]);
+        $family = Category::create([
+            "name"=>"Family",
+            "slug"=>"family",    
+        ]);
+        $work = Category::create([
+            "name"=>"Work",
+            "slug"=>"work",    
+        ]);
+
+        Post::create([
+            "user_id"=>$user->id,
+            "category_id"=>$family->id,
+            "title"=>"My Family Post",    
+            "slug"=>"my-first-post",    
+            "excerpt"=>"Integer leo mi, consectetur eget erat ac, gravida fringilla mauris",    
+            "body"=>"Integer leo mi, consectetur eget erat ac, gravida fringilla mauris. Aliquam libero dui, venenatis rhoncus vestibulum ut, semper sed ex. Praesent suscipit tincidunt nibh vel sollicitudin. Fusce malesuada, est finibus porttitor pellentesque, turpis sapien aliquam nisl, eget sagittis odio libero vitae tortor. Quisque quis libero malesuada, rutrum ex sed, mollis velit. Aenean libero erat, sagittis quis tristique eget, suscipit at est. Sed cursus tortor et lorem pulvinar, at facilisis risus eleifend. Nam laoreet tempor blandit.",    
+        ]);
+        Post::create([
+            "user_id"=>$user->id,
+            "category_id"=>$work->id,
+            "title"=>"My Work Post",    
+            "slug"=>"my-work-post",    
+            "excerpt"=>"Integer leo mi, consectetur eget erat ac, gravida fringilla mauris",    
+            "body"=>"Integer leo mi, consectetur eget erat ac, gravida fringilla mauris. Aliquam libero dui, venenatis rhoncus vestibulum ut, semper sed ex. Praesent suscipit tincidunt nibh vel sollicitudin. Fusce malesuada, est finibus porttitor pellentesque, turpis sapien aliquam nisl, eget sagittis odio libero vitae tortor. Quisque quis libero malesuada, rutrum ex sed, mollis velit. Aenean libero erat, sagittis quis tristique eget, suscipit at est. Sed cursus tortor et lorem pulvinar, at facilisis risus eleifend. Nam laoreet tempor blandit.",    
+        ]);
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
