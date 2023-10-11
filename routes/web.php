4d9a4c09c9a1eeb68f0df4fa17d84f916cd27831
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-11 14:54:15 +0100

    latest post showing

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
     
 
     return view("posts", [
-        "posts" => Post::with("category")->get()
+        "posts" => Post::latest()->with("category")->get()
     ]);
 });
 
