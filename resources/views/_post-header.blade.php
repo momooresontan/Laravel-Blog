5021004d3c2640efde7dfb23041bcfdb7e005576
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-12 14:13:25 +0100

    category dropdown modified(dynamism)

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -18,7 +18,7 @@
 
             <div x-data="{ show: false }" @click.away="show=false">
                 <button @click=" show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left lg:inline-flex">
-                    Categories
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : "Categories" }}
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                     height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
@@ -31,6 +31,10 @@
                 </button>
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-30" style="display: none" >
+                    <a href="/"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                            All
+                        </a>
                     @foreach( $categories as $category )
                         <a href="/categories/{{ $category->slug }}"
                         class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
