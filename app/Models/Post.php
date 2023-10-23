ce3d1e00de668ff6631b7e40417bf4a04e71e931
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-10-23 12:14:51 +0100

    category functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,13 +21,10 @@ class Post extends Model
                 ->where('title', 'like', '%' . $search . '%')
                 ->orWhere('body', 'like', '%' . $search . '%'));
         $query->when($filters['category'] ?? false, fn ( $query, $category ) =>
-            $query
-             ->whereExists(fn($query) => 
-                $query
-                    ->from('categories')
-                    ->whereColumn('categories.id', 'posts.category_id')
-                    ->where('categories.slug', $category) 
-            ));
+            $query->whereHas('category', fn($query)=> $query->where('slug', $category))
+        
+        );
+            
     } 
 
     //protected $fillable = ["title", "excerpt", "slug", "body"];
