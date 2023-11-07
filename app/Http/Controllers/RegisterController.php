4fa70e02de968bf638aeaed54b2031a7aa2030ae
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-07 15:25:13 +0100

    register controller modified

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -11,12 +12,15 @@ class RegisterController extends Controller
     }
 
     public function store(){
-        request()->validate([
+        $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
             'password' => 'required|min:8|max:255',
         ]);
-        dd('Success Validation succeeded');
+
+        User::create($attributes);
+        
+        return redirect('/');
     }
 }
