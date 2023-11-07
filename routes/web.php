644533eb447223f9522c5e29fdea969c34f12921
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-07 12:18:09 +0100

    register controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -22,6 +23,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', [PostController::class, 'index'])->name("home");
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view("posts", [
