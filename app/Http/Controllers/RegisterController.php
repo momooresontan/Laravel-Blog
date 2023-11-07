ab71c7e3cb40816a26143dca276fd3cebd7c4cd1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-07 14:51:47 +0100

    register page modified

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,4 +9,14 @@ class RegisterController extends Controller
     public function create(){
         return view('register.create');
     }
+
+    public function store(){
+        var_dump(request()->all());
+        // request()->validate([
+        //     'name' => 'required|max:255',
+        //     'username' => 'required|max:255|min:3',
+        //     'email' => 'required|email|max:255',
+        //     'password' => 'required|min:8:max:255',
+        // ]);
+    }
 }
