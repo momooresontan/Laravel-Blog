ee713924b4eb0173ccf5f92463d87b17e34814af
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-07 13:11:07 +0100

    register page modified

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -9,25 +9,33 @@
                         Name
                     </label>
                     <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+                </div>
 
+                <div class="mb-6">
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Username
                     </label>
                     <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username" required>
+                </div>
 
+                <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Email
                     </label>
-                    <input type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+                    <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+                </div>
 
+                <div class="mb-6">
                     <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Password
                     </label>
-                    <input type="text" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+                    <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+                </div>
 
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-2"></p>
-                    @enderror
+                <div class="mb-6">
+                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                        Submit
+                    </button>
                 </div>
             </form>
         </main>
