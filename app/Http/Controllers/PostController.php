d5da8842f6286c5e627fd5cab07ddb7b51759ff1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-07 10:15:24 +0100

    pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,16 +9,11 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-
-        return Post::latest()->filter(
-            request(['search', 'category', 'author'])
-        )->get();
-
-        // return view("posts.index", [
-        //     "posts" => Post::latest()->filter(
-        //         request(['search', 'category', 'author'])
-        //     )->paginate(),
-        // ]);
+        return view("posts.index", [
+            "posts" => Post::latest()->filter(
+                request(['search', 'category', 'author'])
+            )->paginate(6),
+        ]);
     }
 
     public function show(Post $post){
