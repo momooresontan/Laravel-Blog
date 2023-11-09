c26af84b6a58c5b68b515797932859a81824206a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-09 11:05:30 +0100

    flash message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,10 +22,12 @@ class RegisterController extends Controller
 
         $attributes['password'] = bcrypt($attributes['password']);
 
-        User::create($attributes);
+        $user = User::create($attributes);
 
         //Flash
         //session()->flash('success', 'Your account has been created');
+
+        auth()->login($user);
         
         return redirect('/')->with('success', 'Your account has been created');
     }
