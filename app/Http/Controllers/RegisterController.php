99ecec4dabd8892553ab1e56ad63196bfed1b65a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-09 08:37:04 +0100

    validation error display

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
     public function store(){
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
+            'username' => 'required|min:3|max:255|unique:users,username',
             'email' => 'required|email|max:255',
             'password' => 'required|min:8|max:255',
         ]);
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -20,6 +20,9 @@
                         Username
                     </label>
                     <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username" value="{{ old('username') }}" required>
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -27,6 +30,9 @@
                         Email
                     </label>
                     <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email"  value="{{ old('email') }}" required>
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -34,6 +40,9 @@
                         Password
                     </label>
                     <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
