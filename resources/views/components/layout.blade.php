519b40c6a0f1007aa392d12c4c4274207a494f6d
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-09 13:45:54 +0100

    logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,10 +15,21 @@
                 </a>
             </div>
 
-            <div class="mt-8 md:mt-0">
-                @guest
+            <div class="mt-8 md:mt-0 flex items-center">
+                @auth
+                    <span href="/register" class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+
+                    <form action="/logout" method="POST" class="text-xs font-semibold text-blue-500 ml-6">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                @else
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                @endguest
+                @endauth
+
+                {{-- @guest
+                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                @endguest --}}
 
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -24,8 +25,10 @@ Route::get('/', [PostController::class, 'index'])->name("home");
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy']);
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view("posts", [
