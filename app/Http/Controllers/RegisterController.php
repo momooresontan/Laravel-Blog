97a8cfa7cac3a1b50dc2cab6ec126fc3c151418a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-11-09 10:55:09 +0100

    flash message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,9 +24,10 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        session()->flash('success', 'Your account has been created');
+        //Flash
+        //session()->flash('success', 'Your account has been created');
         
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created');
     }
 }
 
