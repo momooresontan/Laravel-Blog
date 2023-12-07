161c1b5679ea8c8b3b08ad7cfc271e4eea3b2eb0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-12-07 11:10:49 +0100

    comment functionality & component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted 
+                <time>8 months ago</time>
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti velit, porro quibusdam quos exercitationem autem recusandae ea minima vero, pariatur, eveniet perferendis sint quidem molestiae officiis sed cumque? Numquam, libero. 
+            </p>
+        </header>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,22 +52,7 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10">
-                    <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-                        <div>
-                            <img src="https://i.pravatar.cc/100" alt="">
-                        </div>
-                        <div>
-                            <header>
-                                <h3 class="font-bold">John Doe</h3>
-                                <p class="text-xs">Posted 
-                                    <time>8 months ago</time>
-                                </p>
-                                <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti velit, porro quibusdam quos exercitationem autem recusandae ea minima vero, pariatur, eveniet perferendis sint quidem molestiae officiis sed cumque? Numquam, libero. 
-                                </p>
-                            </header>
-                        </div>
-                    </article>
+                    <x-post-comment />
                 </section>
 
             </article>
