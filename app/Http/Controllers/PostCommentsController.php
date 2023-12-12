6281437f0b1003b03fdbab3c51f0d97c64913fbe
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-12-12 10:45:56 +0100

    comment controller modified (store functiom)

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -8,7 +8,14 @@ use Illuminate\Http\Request;
 class PostCommentsController extends Controller
 {
     public function store(Post $post){
+        request()->validate([
+            'body'=> 'required'
+        ]);
         //add comment to a given post
-        dd($post);
+        $post->comments()->create([
+            'user_id' => request()->user()->id,
+            'body' => request('body')
+        ]);
+        return back();
     }
 }
