45de0c9913fd5cd9a3f52f505ab535922e3fa8f0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-12-12 13:07:59 +0100

    comment controller modified (store function)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,23 +51,22 @@
                     </div>
                 </div>
 
-                <section class="col-span-8 col-start-5 mt-10 space-y-6">
-
-                    <form method="POST" action="/posts/{{ $post->slug }}/comments" class=" border border-gray-200 p-6 rounded-xl">
-                        @csrf
-                        <header class="flex items-center">
-                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
-                            <h2 class="ml-4">Want to participate?</h2>
-                        </header>
-                        <div class="mt-5">
-                            <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Say something"></textarea>
-                        </div>
-                        <div class="flex justify-end mt-5 border-t border-gray-200 pt-3 ">
-                            <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                        </div>
-
-                    </form>
-
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">                  
+                    @auth
+                        <form method="POST" action="/posts/{{ $post->slug }}/comments" class=" border border-gray-200 p-6 rounded-xl">
+                            @csrf
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+                            <div class="mt-5">
+                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Say something"></textarea>
+                            </div>
+                            <div class="flex justify-end mt-5 border-t border-gray-200 pt-3 ">
+                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                            </div>
+                        </form>
+                    @endauth
                     @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
                     @endforeach
