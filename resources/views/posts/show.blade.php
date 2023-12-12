96d43fe9163bef9b3625ea5e1ebfb38bb52e1277
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-12-12 13:18:52 +0100

    comment controller modified (store function)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -66,9 +66,13 @@
                                 <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
                             </div>
                         </form>
+                    @else
+                        <p class="font-bold">
+                            <a href="/login" class="hover:underline">Log in to participate?</a>
+                        </p>
                     @endauth
                     @foreach ($post->comments as $comment)
-                    <x-post-comment :comment="$comment" />
+                        <x-post-comment :comment="$comment" />
                     @endforeach
                 </section>
 
