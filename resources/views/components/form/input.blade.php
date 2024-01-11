f3c2cd378bbc019a1c559e3a439564e94ff953df
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 12:47:16 +0100

    category functionality

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'type' => 'text'])
-<div class="mb-6">
+<x-form.field>
     <x-form.label name="{{ $name }}" />
     <input class="border border-gray-400 p-2 w-full" value="{{ old($name) }}" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" required>
     
    <x-form.error name="{{ $name }}" />
-</div>
+</x-form.field>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,31 +8,22 @@
                 <x-form.input name='slug' />
                 <x-form.input name='thumbnail' type='file' />
                 <x-form.textarea name='excerpt' />
-                
-                <div class="mb-6">
-                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
-                    <textarea class="border border-gray-400 p-2 w-full" name="body" id="body" required>{{ old('body') }}</textarea>
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">Category</label>
-                    <select name="category_id" id="category_id">
-                        @php
-                          $categories = \App\Models\Category::all();  
-                        @endphp
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
+                <x-form.textarea name='body' />
 
-                    </select>
-                    @error('category')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.field>
+                    <x-form.label name="category"/>
+                        <select name="category_id" id="category_id">
+                            @php
+                              $categories = \App\Models\Category::all();  
+                            @endphp
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                                    {{ ucwords($category->name) }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <x-form.error name="category"/>
+                </x-form.field>                    
                 <x-submit-button>Publish</x-submit-button>
             </form>
 
