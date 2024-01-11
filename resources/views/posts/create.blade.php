5c6b5749a14785b6befd1028a29d09c458fb1ff4
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 09:31:35 +0100

    post controller modified(store functionality)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,22 +5,30 @@
                 @csrf
                 <div class="mb-6">
                     <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title" required>
+                    <input class="border border-gray-400 p-2 w-full" value="{{ old('title') }}" type="text" name="title" id="title" required>
                     
                     @error('title')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="mb-6">
+                    <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">Slug</label>
+                    <input class="border border-gray-400 p-2 w-full" value="{{ old('slug') }}" type="text" name="slug" id="slug" required>
+                    
+                    @error('slug')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
                     <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">Excerpt</label>
-                    <textarea class="border border-gray-400 p-2 w-full" name="excerpt" id="excerpt" required></textarea>
+                    <textarea class="border border-gray-400 p-2 w-full" name="excerpt" id="excerpt" required>{{ old('excerpt') }}</textarea>
                     @error('excerpt')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="mb-6">
                     <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
-                    <textarea class="border border-gray-400 p-2 w-full" name="body" id="body" required></textarea>
+                    <textarea class="border border-gray-400 p-2 w-full" name="body" id="body" required>{{ old('body') }}</textarea>
                     @error('body')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
