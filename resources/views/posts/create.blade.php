a39ca9250c5e31af14498f2689e060714b6c0c7d
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 08:48:33 +0100

    admin form creation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,16 +1,22 @@
 <x-layout>
     <section class="px-6 py-8">
-        <x-panel>
+        <x-panel class="max-w-sm mx-auto">
             <form action="/admin/posts" method="POST">
                 @csrf
-    
-                <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
-                <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title" required>
-                
-                @error('title')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
+                <div class="mb-6">
+                    <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title" required>
+                    
+                    @error('title')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">Excerpt</label>
+                </div>
             </form>
+
+
         </x-panel>
     </section>
 </x-layout>
