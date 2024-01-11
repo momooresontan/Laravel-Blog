3531fa5af7870244a9a5105658459edaf852f541
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 08:59:17 +0100

    admin form creation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,7 +13,34 @@
                 </div>
                 <div class="mb-6">
                     <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">Excerpt</label>
+                    <textarea class="border border-gray-400 p-2 w-full" name="excerpt" id="excerpt" required></textarea>
+                    @error('excerpt')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
+                <div class="mb-6">
+                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
+                    <textarea class="border border-gray-400 p-2 w-full" name="body" id="body" required></textarea>
+                    @error('body')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label for="category" class="block mb-2 uppercase font-bold text-xs text-gray-700">Category</label>
+                    <select name="category" id="category">
+                        @php
+                          $categories = \App\Models\Category::all();  
+                        @endphp
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ ucwords($category->name) }}</option>
+                        @endforeach
+
+                    </select>
+                    @error('category')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <x-submit-button>Publish</x-submit-button>
             </form>
 
 
