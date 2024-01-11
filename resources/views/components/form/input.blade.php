74916ca8605cc6858b53b5e52c324a79c7bfa5bb
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 12:02:35 +0100

    form input component

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,9 @@
+@props(['name'])
+<div class="mb-6">
+    <label for="{{ $name }}" class="block mb-2 uppercase font-bold text-xs text-gray-700">{{ ucwords($name) }}</label>
+    <input class="border border-gray-400 p-2 w-full" value="{{ old($name) }}" type="text" name="{{ $name }}" id="{{ $name }}" required>
+    
+    @error($name)
+        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+    @enderror
+</div>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,16 +4,8 @@
         <x-panel>
             <form action="/admin/posts" method="POST" enctype="multipart/form-data">
                 @csrf
-                <x-form-input />
-                
-                <div class="mb-6">
-                    <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">Slug</label>
-                    <input class="border border-gray-400 p-2 w-full" value="{{ old('slug') }}" type="text" name="slug" id="slug" required>
-                    
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name='title' />
+                <x-form.input name='slug' />
 
                 <div class="mb-6">
                     <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">Thumbnail</label>
