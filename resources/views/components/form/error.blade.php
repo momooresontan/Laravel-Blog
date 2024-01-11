d4bfebd6767c20dc83b95da07b9f3cba675b334d
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 12:26:28 +0100

    form error component

---
+++ resources/views/components/form/error.blade.php
@@ -0,0 +1,3 @@
+@error($name)
+    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+@enderror
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,6 +1,6 @@
-@props('name')
+@props(['name'])
 <div class="mb-6">
-    <label for="{{ $name }}" class="block mb-2 uppercase font-bold text-xs text-gray-700">{{ $name }}</label>
+    <label for="{{ $name }}" class="block mb-2 uppercase font-bold text-xs text-gray-700">{{ ucwords($name) }}</label>
     <textarea class="border border-gray-400 p-2 w-full" name="{{ $name }}" id="{{ $name }}" required>{{ old($name) }}</textarea>
     @error($name)
         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
