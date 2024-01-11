19f3c6e54b661eac9701709a5f8de2bf7ef5c945
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-11 12:35:51 +0100

    form label component

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,8 +1,7 @@
 @props(['name'])
 <div class="mb-6">
-    <label for="{{ $name }}" class="block mb-2 uppercase font-bold text-xs text-gray-700">{{ ucwords($name) }}</label>
+    <x-form.label name="{{ $name }}" />
     <textarea class="border border-gray-400 p-2 w-full" name="{{ $name }}" id="{{ $name }}" required>{{ old($name) }}</textarea>
-    @error($name)
-        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-    @enderror
+    
+    <x-form.error name="{{ $name }}" />
 </div>
