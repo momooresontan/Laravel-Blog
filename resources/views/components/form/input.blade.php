c37fdf4eb1d786e170313a2aeef1b1850628d09f
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-12 10:19:11 +0100

    layout blade modified

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'type' => 'text'])
 <x-form.field>
     <x-form.label name="{{ $name }}" />
-    <input class="border border-gray-400 p-2 w-full" value="{{ old($name) }}" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" required>
+    <input class="border border-gray-200 p-2 w-full rounded" value="{{ old($name) }}" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" required>
     
    <x-form.error name="{{ $name }}" />
 </x-form.field>
