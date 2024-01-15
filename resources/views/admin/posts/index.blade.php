aa1da009fcf0985675de61a0944a8c5367625b52
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-15 05:47:55 +0100

    admin index component modified

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,61 +1,55 @@
 <x-layout>
     <x-setting heading="Manage Post">
-        <div class="container mx-auto px-4 sm:px-8">
-            <div class="py-8">
-                <div>
-                    <h2 class="text-2xl font-semibold leading-tight">Users</h2>
-                </div>
-               
-                <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                    <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                        <table class="min-w-full leading-normal">
-                            <tbody>
-                                @foreach($posts as $post)
-                                    <tr>
-                                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                            <div class="flex items-center">
-                                                <div class="ml-3">
-                                                    <p class="text-gray-900 whitespace-no-wrap">
-                                                        {{ $post->title }}
-                                                    </p>
-                                                </div>
+        <div class="flex flex-col">
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block py-2 min-w-full shadow rounded-lg border-t border-gray-200 overflow-hidden">
+                    <table class="min-w-full leading-normal">
+                        <tbody>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm flex-shrink-0s">
+                                        <div class="flex items-center">
+                                            <div class="ml-3">
+                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                    {{ $post->title }}
+                                                </p>
                                             </div>
-                                        </td>
-                                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                            <p class="text-gray-900 whitespace-no-wrap">
-                                                {{ $post->created_at->diffForHumans() }}
-                                            </p>
-                                        </td>
-                                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                            <span
-                                                class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                                <span aria-hidden
-                                                    class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                <span class="relative">Published</span>
-                                            </span>
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-no-wrap text-right text-sm font-medium">
-                                            <a href="#" class="text-blue-500 text-blue-600">Edit</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        <div
-                            class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-                            <span class="text-xs xs:text-sm text-gray-900">
-                                Showing 1 to 4 of 50 Entries
-                            </span>
-                            <div class="inline-flex mt-2 xs:mt-0">
-                                <button
-                                    class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
-                                    Prev
-                                </button>
-                                <button
-                                    class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
-                                    Next
-                                </button>
-                            </div>
+                                        </div>
+                                    </td>
+                                    {{-- <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <p class="text-gray-900 whitespace-no-wrap">
+                                            {{ $post->created_at->diffForHumans() }}
+                                        </p>
+                                    </td> --}}
+                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm flex-shrink-0">
+                                        <span
+                                            class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                            <span aria-hidden
+                                                class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                            <span class="relative">Published</span>
+                                        </span>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm font-medium flex-shrink-0">
+                                        <a href="/admin/posts/{{ $post->id }}/edit" class="text-blue-500 text-blue-600">Edit</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div
+                        class="px-5 py-5 bg-white flex flex-col xs:flex-row items-center xs:justify-between          ">
+                        <span class="text-xs xs:text-sm text-gray-900">
+                            Showing 1 to 4 of 50 Entries
+                        </span>
+                        <div class="inline-flex mt-2 xs:mt-0">
+                            <button
+                                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                                Prev
+                            </button>
+                            <button
+                                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                                Next
+                            </button>
                         </div>
                     </div>
                 </div>
