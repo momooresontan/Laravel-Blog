2837f4ac5db1385f0c7b46ce334778b50bc4e65e
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-15 05:06:15 +0100

    admin index component modified

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -2,7 +2,7 @@
 <section class="py-8 max-w-3xl mx-auto">
     <h1 class="text-lg font-bold mb-8 pb-2 border-b">{{ $heading }}</h1>
     <div class="flex">
-        <aside class="w-48">
+        <aside class="w-48 flex-shrink-0" >
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
